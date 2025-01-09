2f40105703cd016955b0c3d1036a55e4c0fb7e16
zothedev <64105313+zothedev@users.noreply.github.com>
2025-01-09 00:20:17 -0500

    Complete exercise-02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let repeatStr = "";
+    if (num < 0) {
+        return 'ERROR';
+    }
+    while (num >= 1) {
+        repeatStr += str
+        num--;
+    }
+    return repeatStr;
 };
 
 // Do not edit below this line
