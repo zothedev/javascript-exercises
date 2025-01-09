e1b518e9dc899ef4b0b61659a19db9eb2f45a3b5
zothedev <64105313+zothedev@users.noreply.github.com>
2025-01-09 00:31:38 -0500

    Complete exercise-03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let i = str.length-1;
+    let finalStr = "";
+    while (i >= 0) {
+        finalStr += str[i];
+        i--;
+    }
+    return finalStr;
 };
 
 // Do not edit below this line
