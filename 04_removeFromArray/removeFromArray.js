175beebc037c596ed86379e0b1be8a993ad14dec
zothedev <64105313+zothedev@users.noreply.github.com>
2025-01-10 00:07:21 -0500

    Complete exercise-04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(...theArgs) {
+    // ( [1, 2, 3, 4], 3 )
+    let newArray = [];
+    let nums = theArgs.shift();
+    
+    for (let num of nums) {
+        if (theArgs.includes(num)) continue;
+
+        newArray.push(num);
+    }
+    return newArray;
 };
 
 // Do not edit below this line
