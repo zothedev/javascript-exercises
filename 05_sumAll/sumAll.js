ad9734212aac8e23442976b17156f92203b9ae1c
zothedev <64105313+zothedev@users.noreply.github.com>
2025-01-10 00:32:36 -0500

    Complete exercise-05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    let num = 0;
 
+    if ((x < 0 || y < 0) || 
+    (x % 1 !== 0 || y % 1 !== 0) ||
+    (typeof x !== "number") || (typeof y !== "number")) {
+        return 'ERROR';
+    }
+
+    // if y is greater than x, flip the values.
+    if (y < x) {
+        let z = x;
+        x = y;
+        y = z;
+    }
+
+    for (let i = x; i <= y; i++) {
+        num += i;
+    }
+    return num;
 };
 
 // Do not edit below this line
