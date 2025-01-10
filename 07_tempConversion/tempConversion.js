52e8850c0ce380a1d854fb475d9ae5b4d25a6928
zothedev <64105313+zothedev@users.noreply.github.com>
2025-01-10 12:29:51 -0500

    Complete exercise-07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,9 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = (f) => 
+  +((f-32)*(5/9)).toFixed(1);
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = (c) => 
+  +((c*(9/5)+32)).toFixed(1);
+
 
 // Do not edit below this line
 module.exports = {
