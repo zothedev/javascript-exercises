cc3886cd115363cea993848ba99e24d14a48476c
zothedev <64105313+zothedev@users.noreply.github.com>
2025-01-30 14:39:15 -0500

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b; 
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  return arr.reduce((acc, item) => acc + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  return arr.reduce((acc, item) => acc * item, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	if (b === 0) return 1;
+  if (b === 1) return a;
+  let sum = 1;
+  while (b > 0) {
+    sum *= a;
+    b--;
+  }
+  return sum;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0) return 1;
+  let sum = 1;
+	while (a > 0) {
+    sum *= a;
+    a--;
+  }
+  return sum;
 };
 
 // Do not edit below this line
