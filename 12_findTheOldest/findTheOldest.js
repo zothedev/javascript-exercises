d1a3247a281f8fcdc4c17c194e9a6a7128f9e5c0
zothedev <64105313+zothedev@users.noreply.github.com>
2025-02-01 01:43:37 -0500

    Complete exercise 12 - findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let oldestAge = 0;
+    let oldest;
+    arr.forEach((person, index) => {
+        // for each person...calculate their age
+        let age;
+        // if a person doesn't have a death date, use todays year
+        if (!person.yearOfDeath) {
+            age = 2025 - person.yearOfBirth;
+        } else {
+            age = person.yearOfDeath - person.yearOfBirth;
+        }
+        if (age > oldestAge) {
+            oldestAge = age;
+            oldest = person;
+        }
+    });
+    return oldest;
 };
 
 // Do not edit below this line
