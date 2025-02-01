7d391d1bbb152119e87d1b5776ff2d1ec129ad6e
zothedev <64105313+zothedev@users.noreply.github.com>
2025-01-31 20:39:32 -0500

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let acceptableChars = "abcdefghijklmnopqrstuvwxyz0123456789";
+    let isPalindrome = true;
 
+    let cleanedString = str.toLowerCase()
+    .split("")
+    .filter(c => acceptableChars.includes(c))
+    .join("");
+
+    let j = cleanedString.length-1;
+    for (let i = 0; i < j; i++) {
+        if (cleanedString[i] !== cleanedString[j]) {
+            isPalindrome = false;
+        }
+        j--;
+    }
+    return isPalindrome;
 };
 
 // Do not edit below this line
