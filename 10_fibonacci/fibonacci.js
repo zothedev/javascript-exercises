c70c771aeb1633b13ea023f1b40237836f626cf7
zothedev <64105313+zothedev@users.noreply.github.com>
2025-02-01 01:07:28 -0500

    Complete exercise 10 - fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = (+n);
+
+    if (n === 0) return 0;
+    if (n < 0) return "OOPS";
+    
+    let current = 1;
+    let last = 0;
+    let sum = 0;
+    while (n > 1) {
+        sum = current + last;
+        last = current;
+        current = sum;
+        sum = 0;
+        n--;
+    }
+    return current;
 
 };
 
